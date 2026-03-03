a484bf111ea33c8b075490a402303c63a66a962b
Jessica França Moreira <jessicafranca1981@gmail.com>
2026-03-03 17:52:05 -0300

    ci: configura webServer do Vite para execução de testes no GitHub Actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -73,7 +73,11 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  /* Run your local dev server before starting the tests */
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:5173',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
+  },
 });
